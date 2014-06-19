9f3c64c63bfb050766e14ff412637c9db8965b45
davebenradford <dave.ben.radford@gmail.com>
2014-06-19 14:33:50 -0400

    Revert "10th Commit"
    This reverts commit 07884e0a3d7ca6113793000bfecb9524fba50751.

--- src/dbTest/buildDB.java
+++ src/dbTest/buildDB.java
@@ -16,7 +16,7 @@ public class buildDB {
     private static final String hist = "base_historic";
     private static final String conv = "base_conventional";
     private static final String inDB = "spatial";
-    private static final String path = "C:\\Users\\radfordd\\Documents\\NetBeansProjects\\DataBaseTestingProject\\Data\\Spatial\\";
+    private static final String inDBF = "farm2010";
     private static final String[] tbl_names = {"crop_economic_farms", "crop_economic_fields", "crop_economic_subbasins", "forage", "forage_hru", "grazing",
                                                "grazing_economic", "grazing_economic_subbasins", "grazing_hrus", "holding_ponds", "holding_ponds_economic",
                                                "small_dams", "small_dams_economic", "tillage", "tillage_hrus"};
@@ -24,8 +24,7 @@ public class buildDB {
                                                "clay_liner", "plastic_ln", "wire_fence", "distance", "trenching", "pond_yrs", "annual_cost",
                                                "maintenance", "total_cost", "embankment", "life_time", "tillage"};
     private static final String[] types = {"int", "real"};
-    private static final String[] dbf_tbls = {"small_dam.dbf", "cattle_yard.dbf", "grazing.dbf"};
-    //, "land2010_by_land_id", "farm2010"
+    private static final String[] dbf_tbls = {"small_dam", "cattle_yard", "grazing", "land2010_by_land_id", "farm2010"};
     
     /**
      * 
@@ -385,34 +384,7 @@ public class buildDB {
      * @throws CorruptedTableException: Verifies the integrity of the input DBF table. 
      */
     
-    private static void loadDbfTables(Table tbl, String existing, String[] columns) throws IOException {
-        try {
-            tbl.open(IfNonExistent.ERROR);
-            System.out.println("\nOpened " + tbl.getName() + " database successfully");
-            
-            Iterator<Record> iter = tbl.recordIterator();
-            for(int i = 0; i < tbl.getRecordCount(); i++) {
-                Record rec = iter.next();
-                Number exists = rec.getNumberValue(existing);
-                if(exists.intValue() == 1) {
-                    for (String col: columns) {
-                        Number n = rec.getNumberValue(col);
-                        double val = n.doubleValue();
-                        System.out.printf("%.9f", val);
-                        System.out.println(": " + col);
-                    }
-                    System.out.println("\n");
-                }
-            }
-        } catch (IOException | CorruptedTableException e) {
-            Logger.getLogger(buildDB.class.getName()).log(Level.SEVERE, null, e);
-        }
-        finally {
-            tbl.close();
-        }
-    }
-    
-    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException, CorruptedTableException {
         Connection cInDb3 = null, cOutput = null;
         Statement inStmtA = null, inStmtB = null, outStmt =  null;
         Table table = null;
@@ -432,11 +404,56 @@ public class buildDB {
             outStmt = cOutput.createStatement();
             System.out.println("\nConnected established to " + hist + " database successfully");
             
+            table = new Table(new File("C:\\Users\\radfordd\\Documents\\NetBeansProjects\\DataBaseTestingProject\\Data\\Spatial\\" + dbf_tbls[0] + ".dbf"));
+            table.open(IfNonExistent.ERROR);
+            System.out.println("\nOpened " + inDBF + " database successfully");
+            
             buildTables(inStmtA, outStmt);
             System.out.println("\n" + hist + " database created successfully");
             
-            //float num = (float) (4.87e-7 * Math.pow(nv.intValue(), 3.0) - 4.24e-3 * Math.pow(nv.intValue, 2.0) + 1.28e1 * 2682 + 6.71e3);
-            //System.out.println(num / 50);
+            /*
+            Iterator<Record> iter = table.recordIterator();
+            int i = 0;
+            while(i < 26) {
+            String s1 = "Embankment";
+            String s2 = "LifeTime";
+            Record rec = iter.next();
+            double num = 0;
+            Number n1 = rec.getNumberValue(s1);
+            Number n2 = rec.getNumberValue(s2);
+            
+            if(n1 == null) {
+            System.out.println("EMPTY");
+            }
+            else if(n1.doubleValue() % 1 == 0 && !isDouble) {
+            System.out.println(n1.intValue() + ": TYPE = INT");
+            num = (4.87e-7 * Math.pow(n1.intValue(), 3.0) - 4.24e-3 * Math.pow(n1.intValue(), 2.0) + 1.28e1 * 2682 + 6.71e3);
+            System.out.printf("%.2f", num / 50);
+            System.out.println();
+            }
+            else {
+            isDouble = true;
+            System.out.println(n1 + ": TYPE = DOUBLE");
+            }
+            if(n2 == null) {
+            System.out.println("EMPTY");
+            }
+            else if(n2.doubleValue() % 1 == 0 && !isDouble) {
+            System.out.println(n2.intValue() + ": TYPE = INT");
+            num = (4.87e-7 * Math.pow(n2.intValue(), 3.0) - 4.24e-3 * Math.pow(n2.intValue(), 2.0) + 1.28e1 * 2682 + 6.71e3);
+            System.out.printf("%.2f", num / 50);
+            System.out.println(n2);
+            }
+            else {
+            isDouble = true;
+            System.out.println(n2 + ": TYPE = DOUBLE");
+            }
+            i++;
+            }
+            
+            float num = (float) (4.87e-7 * Math.pow(nv.intValue(), 3.0) - 4.24e-3 * Math.pow(nv.intValue, 2.0) + 1.28e1 * 2682 + 6.71e3);
+            System.out.println(num / 50);
+            */
             
             String tbl = "yield_historic";
             fillCropEconFields(inStmtA, outStmt, tbl);
@@ -448,107 +465,62 @@ public class buildDB {
             fillCropEconSubbasins(inStmtA, inStmtB, outStmt, tbl, tblB);
             System.out.println("\ncrop_economic_subbasins database created successfully");
             
-            String[] dam = {"ID", "Embankment", "LifeTime"};
-            loadDbfTables(new Table(new File(path + dbf_tbls[0])), "Existing", dam);
-            String[] pond = {"ID", "HRU", "Cattles", "ClayLiner", "PlasticLn", "WireFence", "Distance", "Trenching", "Pond_Yrs"};
-            loadDbfTables(new Table(new File(path + dbf_tbls[1])), "Existing", pond);
-            String[] graze = {"ID", "Grazing_Ha", "UnitCost"};
-            loadDbfTables(new Table(new File(path + dbf_tbls[2])), "Existing", graze);
-                /*
-                int i = 0;
-                while(i < 26) {
-                String s1 = "Embankment";
-                String s2 = "LifeTime";
-                Record rec = iter.next();
-                double num = 0;
-                Number n1 = rec.getNumberValue(s1);
-                Number n2 = rec.getNumberValue(s2);
+            /** HOLDING POND COST / Lifetime (50)
+                            double sqrtCattles = Math.Sqrt(Cattles);
+                double temp3 = 2.232 * Cattles + 11.338 * sqrtCattles;
+                double temp = 3.72 * Cattles + _trenching * 7.94 * sqrtCattles + 0.844 * Distance +
+                    _clay_liner * temp3;
 
-                if(n1 == null) {
-                System.out.println("EMPTY");
-                }
-                else if(n1.doubleValue() % 1 == 0 && !isDouble) {
-                System.out.println(n1.intValue() + ": TYPE = INT");
-                num = (4.87e-7 * Math.pow(n1.intValue(), 3.0) - 4.24e-3 * Math.pow(n1.intValue(), 2.0) + 1.28e1 * 2682 + 6.71e3);
-                System.out.printf("%.2f", num / 50);
-                System.out.println();
-                }
-                else {
-                isDouble = true;
-                System.out.println(n1 + ": TYPE = DOUBLE");
-                }
-                if(n2 == null) {
-                System.out.println("EMPTY");
-                }
-                else if(n2.doubleValue() % 1 == 0 && !isDouble) {
-                System.out.println(n2.intValue() + ": TYPE = INT");
-                num = (4.87e-7 * Math.pow(n2.intValue(), 3.0) - 4.24e-3 * Math.pow(n2.intValue(), 2.0) + 1.28e1 * 2682 + 6.71e3);
-                System.out.println(num / 50);
-                System.out.println(n2);
-                }
-                else {
-                isDouble = true;
-                System.out.println(n2 + ": TYPE = DOUBLE");
-                }
-                i++;
-                }
+                double temp2 = (0.5 * 9.5 + 7.47) * temp3;
+
+                double max = 1.38e-10 * Math.Pow(temp,2.0) 
+                    - 5.027e-5 * temp 
+                    + 6.736 + _clay_liner * temp2
+                    + _plastic_liner / 0.7 * temp2
+                    + _wire_fence * (189.0 + Math.Sqrt(820.0 * Cattles))
+                    + 10000.0;
+                max *= 1.1483;
+
+                temp3 = 1.512 * Cattles + 9.332 * sqrtCattles;
+                temp = 2.52 * Cattles + _trenching * 6.54 * sqrtCattles + 0.844 * Distance +
+                    _clay_liner * temp3;
+                temp2 = (0.5 * 9.5 + 7.47) * temp3;
+                double min = 1.38e-10 * Math.Pow(temp, 2.0)
+                    - 5.027e-5 * temp
+                    + 6.736 + _clay_liner * temp2
+                    + _plastic_liner / 0.7 * temp2
+                    + _wire_fence * (189.0 + Math.Sqrt(556.0 * Cattles))
+                    + 10000.0;
+                min *= 1.1483;
+
+                return min / 2.0 + max / 2.0; 
                 */
-                
-                /** HOLDING POND COST / Lifetime (50)
-                 * double sqrtCattles = Math.Sqrt(Cattles);
-                 * double temp3 = 2.232 * Cattles + 11.338 * sqrtCattles;
-                 * double temp = 3.72 * Cattles + _trenching * 7.94 * sqrtCattles + 0.844 * Distance +
-                 * _clay_liner * temp3;
-                 * 
-                 * double temp2 = (0.5 * 9.5 + 7.47) * temp3;
-                 * 
-                 * double max = 1.38e-10 * Math.Pow(temp,2.0)
-                 * - 5.027e-5 * temp
-                 * + 6.736 + _clay_liner * temp2
-                 * + _plastic_liner / 0.7 * temp2
-                 * + _wire_fence * (189.0 + Math.Sqrt(820.0 * Cattles))
-                 * + 10000.0;
-                 * max *= 1.1483;
-                 * 
-                 * temp3 = 1.512 * Cattles + 9.332 * sqrtCattles;
-                 * temp = 2.52 * Cattles + _trenching * 6.54 * sqrtCattles + 0.844 * Distance +
-                 * _clay_liner * temp3;
-                 * temp2 = (0.5 * 9.5 + 7.47) * temp3;
-                 * double min = 1.38e-10 * Math.Pow(temp, 2.0)
-                 * - 5.027e-5 * temp
-                 * + 6.736 + _clay_liner * temp2
-                 * + _plastic_liner / 0.7 * temp2
-                 * + _wire_fence * (189.0 + Math.Sqrt(556.0 * Cattles))
-                 * + 10000.0;
-                 * min *= 1.1483;
-                 * 
-                 * return min / 2.0 + max / 2.0;
-                 */
-                
-                /** HOLDING POND MAINTENANCE COST
-                 *                 double temp = 0.03048 * Math.Pow(Math.Sqrt(1.68 * Cattles) - 6,2.0);
-                 * double min = 1.38e-10 * Math.Pow(temp, 2.0)
-                 * - 5.027e-5 * temp
-                 * + 6.737
-                 * + _wire_fence * (24.48 + 3.05 * Math.Sqrt(Cattles))
-                 * + 1.25 * Cattles;
-                 * 
-                 * temp = 0.03048 * Math.Pow(Math.Sqrt(2.48 * Cattles) - 6, 2.0);
-                 * double max = 1.38e-10 * Math.Pow(temp, 2.0)
-                 * - 5.027e-5 * temp
-                 * + 6.737
-                 * + _wire_fence * (24.48 + 3.71 * Math.Sqrt(Cattles))
-                 * + 1.85 * Cattles;
-                 * 
-                 * return min / 2.0 + max / 2.0;
-                 */
-                // HOLDING POND TOTALCOST = ANNUAL + MAINTENANCE
-                // HOLDING_ECON COST == TOTALCOST
-                
-                // GRAZING COST = UNIT COST * AREA from DBF
+            
+            /** HOLDING POND MAINTENANCE COST
+             *                 double temp = 0.03048 * Math.Pow(Math.Sqrt(1.68 * Cattles) - 6,2.0);
+                double min = 1.38e-10 * Math.Pow(temp, 2.0)
+                    - 5.027e-5 * temp
+                    + 6.737
+                    + _wire_fence * (24.48 + 3.05 * Math.Sqrt(Cattles))
+                    + 1.25 * Cattles;
+
+                temp = 0.03048 * Math.Pow(Math.Sqrt(2.48 * Cattles) - 6, 2.0);
+                double max = 1.38e-10 * Math.Pow(temp, 2.0)
+                    - 5.027e-5 * temp
+                    + 6.737
+                    + _wire_fence * (24.48 + 3.71 * Math.Sqrt(Cattles))
+                    + 1.85 * Cattles;
+
+                return min / 2.0 + max / 2.0; 
+                */
+            // HOLDING POND TOTALCOST = ANNUAL + MAINTENANCE
+            // HOLDING_ECON COST == TOTALCOST
+            
+            // GRAZING COST = UNIT COST * AREA from DBF
         } catch(SQLException e) {
             System.out.println(e);
         } finally {
+            table.close();
             cOutput.commit();
             cOutput.close();
         }
